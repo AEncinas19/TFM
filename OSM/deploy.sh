45b9c51b21adb75e85d561e8689456421ccb553b
AEncinas19 <49205996+AEncinas19@users.noreply.github.com>
2023-03-01 12:41:53 +0100

    Update deploy.sh

--- OSM/deploy.sh
+++ OSM/deploy.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-microk8s enable registry
-sudo touch /etc/docker/daemon.json
-echo '{"insecure-registries": ["192.168.56.11:32000"]}' | sudo tee /etc/docker/daemon.json
-sudo systemctl restart docker
-cd img/vnf-img
-docker build . -t 192.168.56.11:32000/vnf-image:latest
-docker push 192.168.56.11:32000/vnf-image:latest
-
-cd ../..
-
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64_mejora.xml -t
 sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -t
 xhost +
 
-ssh upm@192.168.56.12 "./shared/rdsv-final/osm_conf.sh"
+ssh upm@192.168.56.12 "./TFM/OSM/osm_conf.sh"
 
 exit 0
 
