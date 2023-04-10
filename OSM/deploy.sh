2966674ff8b7ee5f70a2fc83795be78a9d8dc335
AEncinas19 <49205996+AEncinas19@users.noreply.github.com>
2023-04-10 17:51:26 +0200

    Update deploy.sh

--- OSM/deploy.sh
+++ OSM/deploy.sh
@@ -6,6 +6,7 @@ sudo touch /etc/docker/daemon.json
 echo '{"insecure-registries": ["192.168.56.11:32000"]}' | sudo tee /etc/docker/daemon.json
 
 sudo systemctl restart docker
+sudo docker tag ceos:4.29.0.2F 192.168.56.11:32000/ceos:latest
 sudo docker push 192.168.56.11:32000/ceos:latest
 
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -t
