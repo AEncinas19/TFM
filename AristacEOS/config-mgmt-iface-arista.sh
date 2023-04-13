b7280770ba6ed1ea69e279b1e45f82a9140a9242
AEncinas19 <aitor.encinas.alonso@gmail.com>
2023-04-13 10:15:57 +0200

    Configurar IP de interfaz de gestión Arista

--- AristacEOS/config-mgmt-iface-arista.sh
+++ AristacEOS/config-mgmt-iface-arista.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pod_name="$1"
 IP_address="$2"
-kuecetl exec -it $pod_name -- Cli -p 15 -c "configure terminal
+kubectl exec -it $pod_name -- Cli -p 15 -c "configure terminal
 interface Management0
 ip address $IP_address
+end
 "
