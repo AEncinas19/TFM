734ce1740b8df281b343f2cc926d3ad46f7f7070
AEncinas19 <aitor.encinas.alonso@gmail.com>
2023-04-13 12:53:44 +0200

    Arreglando fichero

--- AristacEOS/config-mgmt-iface-arista.sh
+++ AristacEOS/config-mgmt-iface-arista.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+OSMNS="$3"
 pod_name="$1"
-IP_address="$2"
-kubectl exec -it $pod_name -- Cli -p 15 -c "configure terminal
+IP_address="$2"/24
+kubectl -n $OSMNS exec -it $pod_name -- Cli -p 15 -c "configure terminal
 interface Management0
 ip address $IP_address
 end
+
 "
