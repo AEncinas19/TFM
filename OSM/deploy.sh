592314fa95a0c6ad2eb049871320bf9e2117d83b
AEncinas19 <49205996+AEncinas19@users.noreply.github.com>
2023-04-18 10:03:18 +0200

    Update deploy.sh

--- OSM/deploy.sh
+++ OSM/deploy.sh
@@ -10,11 +10,12 @@ sleep 15
 sudo docker tag ceos:4.29.0.2F 192.168.56.11:32000/ceos:latest
 sudo docker push 192.168.56.11:32000/ceos:latest
 
+sudo ovs-vsctl add-br PodNet
+sudo ifconfig PodNet up
+
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -t
 sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -t
 xhost +
-
-ssh upm@192.168.56.12 "./TFM/OSM/osm_conf.sh"
 
 exit 0
 
