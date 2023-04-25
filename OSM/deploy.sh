4245391e19e7443f46ec13cc371f1dd00b6a7c7b
AEncinas19 <49205996+AEncinas19@users.noreply.github.com>
2023-04-25 11:27:08 +0200

    Update deploy.sh

--- OSM/deploy.sh
+++ OSM/deploy.sh
@@ -7,12 +7,16 @@ echo '{"insecure-registries": ["192.168.56.11:32000"]}' | sudo tee /etc/docker/d
 
 sudo systemctl restart docker
 sleep 15
+
+cd img/vnf-router
+sudo docker build -t ceos:4.29.0.2F
 sudo docker tag ceos:4.29.0.2F 192.168.56.11:32000/ceos:latest
 sudo docker push 192.168.56.11:32000/ceos:latest
 
 sudo ovs-vsctl add-br PodNet
 sudo ifconfig PodNet up
 
+cd ../..
 sudo vnx -f vnx/nfv3_home_lxc_ubuntu64.xml -t
 sudo vnx -f vnx/nfv3_server_lxc_ubuntu64.xml -t
 xhost +
