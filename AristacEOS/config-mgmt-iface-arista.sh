d2ee982ac3d6772306c7ee58296049afc7ef29dd
AEncinas19 <49205996+AEncinas19@users.noreply.github.com>
2023-05-04 12:50:32 +0200

    Update config-mgmt-iface-arista.sh

--- AristacEOS/config-mgmt-iface-arista.sh
+++ AristacEOS/config-mgmt-iface-arista.sh
@@ -2,7 +2,7 @@
 OSMNS="$3"
 pod_name="$1"
 IP_address="$2"/24
-kubectl -n $OSMNS exec -it $pod_name -- Cli -p 15 -c "configure terminal
+microk8s kubectl -n $OSMNS exec -it $pod_name -- Cli -p 15 -c "configure terminal
 interface Management0
 ip address $IP_address
 end
