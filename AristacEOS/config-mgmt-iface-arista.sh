a45523fb86c50880a5bd772270c4c9ba8234fd52
AEncinas19 <aitor.encinas.alonso@gmail.com>
2023-05-06 13:58:07 +0200

    Automatizando NAT y ocprometheus.yml

--- AristacEOS/config-mgmt-iface-arista.sh
+++ AristacEOS/config-mgmt-iface-arista.sh
@@ -2,9 +2,9 @@
 OSMNS="$3"
 pod_name="$1"
 IP_address="$2"/24
+microk8s kubectl -n $OSMNS exec -it $pod_name -- Cli -p 15 -c "bash ./mnt/flash/vnx_config_nat vlan1 eth2"
 microk8s kubectl -n $OSMNS exec -it $pod_name -- Cli -p 15 -c "configure terminal
 interface Management0
 ip address $IP_address
 end
-
 "
